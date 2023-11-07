49e35e75a9b2c9774fcea2f05f840aaa58eb34f7
Timo Wacke <wacketimo@gmail.com>
2023-11-07 15:42:25 +0100

    aufgabe3 transfer to other machine

---
+++ Splines.py
@@ -0,0 +1,12 @@
+# imports for the whole file
+from numpy import *
+from scipy import *
+
+
+'''
+    Function that we want to interpolate.
+    Given in the Exercise.
+'''
+def f(x):
+    return exp(-x**2)
+

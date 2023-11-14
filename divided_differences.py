715744f2a955049991250816a46683863daa9bf7
Timo Wacke <timo.wacke@ommax.de>
2023-11-14 18:20:39 +0100

    restrucutred, init notebook

---
+++ divided_differences.py
@@ -0,0 +1,111 @@
+'''
+    Stuetzstelle Class, StuetzstellenListe Class and TriangularRecursive
+Schema Class are useful for
+    the recursive scheme...
+
+    With the DividedDifferences
+ Class that is a subclass of TriangularRecursive
+Schema
+    and the Newton Class we have the tools to interpolate a given function with not only
+    Newton Interpolation but also with minimal effort with Hermite Interpolation or Tschebyscheff Interpolation
+
+    These classes have been used in a previous exercise already
+'''
+
+
+class SupportingPoint:
+    def __init__(self, x, f):
+        self.x = x
+        self.f = f
+
+    def __str__(self):
+        return f'f({self.x}) = {self.f:.2f}'
+
+class SupportPointList:
+    def __init__(self):
+        self.list = []
+
+    def add(self, sp: SupportingPoint):
+        self.list.append(sp)
+
+    def get(self, k, j=0):
+        if (k-j) < 0:
+            print('k-j should not be negative!')
+            return None
+        elif (k-j) >= len(self.list):
+            print('k-j should not be greater than the length of the list!')
+            return None
+        else:
+            return self.list[k-j]
+
+class TriangularRecursiveSchema:
+    def __init__(self, spl: SupportPointList):
+        self.spl = spl
+        if len(self.spl.liste) > 0:
+            # init 2d array
+            self._k_js = [[None for i in range(j+1)] for j in range(len(spl.liste))]
+        else:
+            self._k_js = []
+        
+    def expand_size_to(self, n):
+        # add empty rows to the 2d array
+        for i in range(len(self._k_js), n):
+            self._k_js.append([None for i in range(i+1)])
+
+    def notation(self, k, j):
+        return f'D_{k}_{j}'
+
+    def calc_kj(self, k, j):
+        if j == 0:
+            self._k_js[k][j] = self.spl.get(k).f
+        elif self._k_js[k][j] is None:
+            _k_minus_1_j_minus_1 = self.calc_kj(k-1, j-1)
+            _k_j_minus_1 = self.calc_kj(k, j-1)
+            x_k = self.spl.get(k).x
+            x_k_minus_j = self.spl.get(k-j).x
+            _k_j = self.recursion_rule(_k_j_minus_1, _k_minus_1_j_minus_1, x_k, x_k_minus_j)
+            assert _k_j is not None
+            self._k_js[k][j] = _k_j
+        return self._k_js[k][j]
+
+
+    def visualize(self):
+        n = len(self.spl.liste) - 1
+        # print table
+        print('k\\j', end='\t\t')
+        for j in range(n+1):
+            # print header with j's , no line break
+            print(f'j={j}', end='\t\t') # 0, 1, ..., n
+        print('\n')
+        for k in range(n+1): # 0, 1, ..., n
+            # print k's	
+            print(f'k={k}, {self.spl.get(k).x:.2f}', end='\t')
+            for j in range(k+1): # 0, 1, ..., k
+                print(f'{self.notation(k, j)}={self._k_js[k][j]:.2f}', end='\t')
+            print('')
+        print(f'\n{self.notation(n, n)} = {self._k_js[n][n]}\n')
+        print('----------------------------------------\n\n')
+
+
+class DividedDifferences(TriangularRecursiveSchema):
+    def __init__(self, spl):
+        super().__init__(spl)
+
+    def notation(self, k, j):
+        if j == 0:
+            return f'f[x{k}]'
+        if j == 1:
+            return f'f[x{k}, x{k-1}]'
+        return f'f[x{k}..x{k-j}]'
+
+    def recursion_rule(self, dd_k_j_minus_1, dd_k_minus_1_j_minus_1, x_k, x_k_minus_j):
+        dd_k_j = (dd_k_j_minus_1 - dd_k_minus_1_j_minus_1) / (x_k - x_k_minus_j)
+        return dd_k_j
+
+    def dds_nth_grade(self, n, visualize=False):
+        assert n < len(self.spl.liste) and n >= 0, self.n_out_of_bounds(n)
+        dd_k_j = self.calc_kj(n, n)
+        if visualize:
+            print(f'Calculating divided difference: {self.notation(n, n)}')
+            self.visualize()
+        return dd_k_j
---
+++ functions.py
@@ -0,0 +1,189 @@
+'''
+    class that can run backpropagation on a MathExpression to help us
+    compute the derivative of a function at a given point
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MathExpression:
+    # value is the result of the math expression
+    # children can be other MathExpressions or numbers
+    # operator can be used for printing / representation
+    def __init__(self, value, children=(), operator=''):
+        self.value = value
+        self.children = children
+        self.operator = operator
+        '''
+        grad will be set by backpropagation as the partial derivative,
+        of this expression relative to one of its later parents
+        '''
+        self.grad = 0 
+        self._backward = lambda: None 
+
+    def __add__(self, other):
+        if not isinstance(other, MathExpression):
+            other = MathExpression(other, (), 'const')
+        out = MathExpression(self.value + other.value, (self, other), '+')
+        
+        def backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = backward
+
+        return out
+
+    def __mul__(self, other):
+        if not isinstance(other, MathExpression):
+            other = MathExpression(other, (), 'const')
+        out = MathExpression(self.value * other.value, (self, other), '*')
+
+        def backward():
+            self.grad += other * out.grad
+            other.grad += self * out.grad
+
+        out._backward = backward
+        return out    
+
+    def __pow__(self, other):
+        # other needs to be constant
+        assert isinstance(other, (int, float))
+        other = MathExpression(other, (), 'const')
+        out = MathExpression(self.value ** other.value, (self, other), '**')
+
+        def backward():
+            self.grad += other * (self ** (other.value - 1)) * out.grad
+        out._backward = backward
+        return out
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __rsub__(self, other):
+        return -self + other
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def exp(self):
+        out = MathExpression(np.exp(self.value), (self,), 'exp')
+
+        def backward():
+            self.grad += out.grad * self.exp()
+        out._backward = backward
+        return out
+    
+    def backward(self):
+         # topological order all of the children in the graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v.children:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        # go one variable at a time and apply the chain rule to get its gradient
+        self.grad = MathExpression(1, (), 'const')
+        for v in reversed(topo):
+            v._backward()
+  
+    
+    def reset_grad(self):
+        self.grad = 0
+        for child in self.children:
+            child.reset_grad()
+    
+    def __repr__(self):
+        if self.operator == 'const':
+            return f'Math({self.value})'
+        elif self.operator.startswith('var='):
+            return f'Math({self.operator.split("=")[1]})={self.value}'
+        elif len(self.children) == 1:
+            return f'Math({self.operator}{self.children[0]})={self.value}'
+        else:
+            return f'Math({self.children[0]} {self.operator} {self.children[1]})={self.value}'
+    
+
+'''
+    class that enables derivation of a Functional at a given point
+    using the MathExpression class which implements backpropagation
+'''
+class FunctionOverX:
+    def __init__(self, function):
+        self.function = function
+
+    def __call__(self, x):
+        x = MathExpression(x, (), 'var=x')
+        return self.function(x).value
+    
+    '''
+        calulates (d/dx)^n at self.f(x), arguments: x, n:int 
+    '''
+    def n_th_derivative_at(self, x, n=1):
+        x = MathExpression(x, (), 'var=x')
+        y = self.function(x)
+        i_th_derivative = y
+        yield i_th_derivative.value
+        for _ in range(n):
+            i_th_derivative.reset_grad()
+            i_th_derivative.backward()
+            i_th_derivative = x.grad
+            yield i_th_derivative.value
+    
+    def n_th_derivative(self, n=1):
+        return lambda x: self.n_th_dertivative_at(x, n)
+
+
+'''
+    Class that makes it easy to plot multiple callables
+    over a given interval with a given resolution
+
+    Usage Example:
+    plotter = PlotFunctions([('f', f), ('spline', spline.spline, x_supports, y_support)], spline.x_list, spline.y_list)
+
+    x_supports and y_supports are optional and can be used to plot the support points of the spline
+'''  
+
+class PlotFunctions:
+    def __init__(self, labled_functions):
+        self.labled_functions = []
+        for f in labled_functions:
+            assert len(f) >= 2
+            assert isinstance(f[0], str)
+            assert callable(f[1])
+            if len(f) == 2:
+                self.labled_functions.append((f[0], f[1], None, None))
+            else:
+                assert len(f) == 4
+                self.labled_functions.append((f[0], f[1], f[2], f[3]))
+
+
+    
+    def plot(self, a=-5, b=5, resolution=10):
+        # resolution: drawn points per unit
+        # xs length
+        n = int(b - a) * resolution
+        xs = np.linspace(a, b, n)
+
+        # plot the function
+        for label, function, xp, yp in self.labled_functions:
+            # generate y values
+            ys = []
+            for x in xs:
+                ys.append(function(x))
+            plt.plot(xs, ys, label=label)
+            if xp is not None and yp is not None:
+                plt.plot(xp, yp, 'o', label='data points for ' + label)
+        plt.legend()
+        plt.show()
+        
